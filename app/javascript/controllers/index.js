b6b7155d8b370cb3751ae08eb8b16dedbb90654f
seijitakahashi <takahashiseiji18@gmail.com>
2025-04-12 15:38:53 -0600

    package fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,12 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// Eager load all controllers defined in the controllers directory
+const controllers = import.meta.glob("./**/*_controller.js", { eager: true })
+Object.entries(controllers).forEach(([filename, module]) => {
+  const name = filename
+    .replace("./", "")
+    .replace("_controller.js", "")
+    .replace("/", "--")
+  application.register(name, module.default)
+})
