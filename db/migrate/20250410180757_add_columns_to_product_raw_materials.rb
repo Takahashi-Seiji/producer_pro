a844122321060666764ee43c3f5b055527c6ec9f
seijitakahashi <takahashiseiji18@gmail.com>
2025-04-12 16:00:07 -0600

    delete migr

--- db/migrate/20250410180757_add_columns_to_product_raw_materials.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToProductRawMaterials < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :product_raw_materials, :product, null: false, foreign_key: true
-    add_reference :product_raw_materials, :raw_material, null: false, foreign_key: true
-    add_column :product_raw_materials, :quantity_required, :decimal, precision: 10, scale: 2, null: false
-    add_column :product_raw_materials, :unit, :string, null: false
-    add_index :product_raw_materials, [:product_id, :raw_material_id], unique: true
-  end
-end
